f2f3c4e66761101dd40aff0cb6a64d377493ba7e
zhangweichao <770957815@qq.com>
2018-10-11 22:04:19 +0800

    add

---
+++ Flask_backstage/app.py
@@ -0,0 +1,30 @@
+from flask import Flask,redirect,url_for,render_template
+import config
+
+app = Flask(__name__)
+app.config.from_object(config)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/login/')
+def login():
+    return  render_template('login.html')
+
+@app.route('/register/')
+def register():
+    return  "jcsausao"
+
+@app.route('/reservation/')
+def reservation():
+    return  "sfsafsaf"
+@app.route('/room_info/')
+def room_info():
+    return  "fsaf"
+
+
+
+if __name__ == '__main__':
+    app.run()
